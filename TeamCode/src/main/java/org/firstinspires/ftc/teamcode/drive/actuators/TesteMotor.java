2adeede50534fcd51745895fd2daaf33055caf47
Davi-coder123 <davif.camillo@gmail.com>
2025-11-12 16:21:59 -0300

    Mudou o TesteMotor para 2 motores

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/actuators/TesteMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/actuators/TesteMotor.java
@@ -8,20 +8,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="Teste Motor", group="Testes")
 public class TesteMotor extends OpMode {
 
-    DcMotor motor;
+    DcMotor motor, motor1;
 
     @Override
     public void init() {
         // nome do motor deve ser igual ao configurado no Driver Hub
         motor = hardwareMap.get(DcMotor.class, "RMTa");
+        motor1 = hardwareMap.get(DcMotor.class, "RMX");
         telemetry.addData("Status", "Inicializado");
     }
 
     @Override
     public void loop() {
         // controlado pelo gatilho direito e esquerdo do gamepad
-        double power = gamepad1.right_trigger;
+        double power = gamepad1.right_trigger - gamepad1.left_trigger;
         motor.setPower(power);
+        motor1.setPower(-power);
 
         telemetry.addData("Potência", power);
         telemetry.update();
